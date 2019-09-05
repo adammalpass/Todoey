e2777ca09ca380e1a3b206673d21fb4d93dd0073
Adam Malpass <adammalpass@hotmail.co.uk>
2019-09-05 21:39:12 +0900

    Add search bar

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -151,7 +151,7 @@ class TodoListViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    func loadItems()
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest())
     {
 //        if let data = try? Data(contentsOf: dataFilePath!)
 //        {
@@ -166,15 +166,56 @@ class TodoListViewController: UITableViewController {
 //            }
 //        }
         
-        let request : NSFetchRequest<Item> = Item.fetchRequest()
         do
         {
             itemArray = try context.fetch(request)
+            tableView.reloadData()
         }
         catch
         {
             print("Error fetching data from context \(error)")
         }
+    }
+}
+
+//MARK: - Search bar methods
+
+extension TodoListViewController: UISearchBarDelegate
+{
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        print(searchBar.text!)
+        
+        searchItems(in: searchBar)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchBar.text?.count == 0
+        {
+            loadItems()
+            
+            DispatchQueue.main.async
+            {
+                searchBar.resignFirstResponder()
+            }
+            
+        }
+        else
+        {
+            searchItems(in: searchBar)
+        }
+    }
+    
+    func searchItems(in searchBar: UISearchBar)
+    {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
     }
     
 }
