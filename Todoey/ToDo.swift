564e4d878ef92b988cbff08bc6641658c139ea09
Adam Malpass <adammalpass@hotmail.co.uk>
2019-09-07 11:17:56 +0900

    New branch to persist data using Realm instead of CoreData

---
+++ Todoey/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  Todoey
+//
+//  Created by Adam Malpass on 2019/09/01.
+//  Copyright © 2019 Adam Malpass. All rights reserved.
+//
+
+import Foundation
+
+class ToDo
+{
+    var title : String = ""
+    var done : Bool = false
+}
